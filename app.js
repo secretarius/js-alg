c53626204ad1cb676fcb2db85dcee612e7c605a4
Alex <olexandrmai@gmail.com>
2022-09-05 17:47:52 +0200

    time complexity

---
+++ app.js
@@ -0,0 +1,34 @@
+// function sumNumbers(numbers) {
+//     return numbers[0] + numbers[1] + numbers[2]; //0(1)
+// }
+
+// numbers = [ 1, 3, 10, 15]
+
+function sumNumbers(numbers) {
+    let sum = 0;
+    for( let i = 0; i <= numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum
+    
+}
+
+console.log(sumNumbers([1, 3, 10]));
+
+
+
+function sumNumber(numbers) {
+    let sum = 0; //1
+    for( let value of numbers) {
+        console.log('...'); //4
+        sum = sum + value; //4 => n
+    }
+    return sum; // 1
+    //return numbers.reduce((sum, curNum) => sum + curNum, 0);
+}
+
+// T = 1 + 1 + 1 + n = n = 3n + 2n = 3 + 2*n
+// T = 2*n
+// T = n => 0(n) => Linar Time Complexity
+
+console.log(sumNumber([1, 3, 10, 50]));
